d3e0a72d899b6a25f7c1d1461f0d8ac51ca499d2
Vlad <dr.melix@gmail.com>
2024-05-13 22:52:56 +0300

    Удалены лишние файлы

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import requests
 
+# Глобальные константы
+LOCATIONS = ["Лондон", "Шереметьево", "Череповец"]
 
-def get_weather(locations_list, lang='en'):
+
+def get_weather(locations_list, lang='ru'):
     for loc in locations_list:
         params = {'nTqu': '', 'lang': lang}
         url = f"https://wttr.in/{loc}"
         response = requests.get(url, params=params)
         if response.status_code == 200:
-            print(f"Weather for {loc}:")
+            print(f"Погода для {loc}:")
             print(response.text)
         else:
-            print(f"Failed to get weather data for {loc}, status code: {response.status_code}")
+            print(f"Не удалось получить данные о погоде для {loc}, статус код: {response.status_code}")
 
 
-# Запрос погоды для списка местоположений на английском и русском языках
-locations = ["London", "SVO", "Cherepovets"]
-get_weather(locations, lang='en')  # Погода на английском языке
-get_weather(['Череповец'], lang='ru')  # Погода для Череповца на русском языке
+def main():
+    get_weather(LOCATIONS, lang='ru')
+
+
+if __name__ == "__main__":
+    main()
